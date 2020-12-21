f3cc61b1d0b48f4a40c0c360e20aff07e2ade312
Marc J <john.doe.gmail.com>
2020-12-21 14:30:36 +0100

    JpaArtikelRepositoryTest

---
+++ src/test/java/be/vdab/allesvoordekeuken/repositories/JpaArtikelRepositoryTest.java
@@ -0,0 +1,38 @@
+package be.vdab.allesvoordekeuken.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(JpaArtikelRepository.class)
+@Sql("/insertArtikel.sql")
+public class JpaArtikelRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+    private final JpaArtikelRepository repository;
+
+
+    public JpaArtikelRepositoryTest(JpaArtikelRepository repository) {
+        this.repository = repository;
+    }
+
+    private long idVanTestArtikel(){
+        return super.jdbcTemplate.queryForObject(
+                "select id from artikels where naam = 'test'", long.class
+        );
+    }
+
+    @Test
+    void findById(){
+        assertThat(repository.findById(idVanTestArtikel()).get().getNaam())
+        .isEqualTo("test");
+    }
+
+    @Test
+    void findByOnbestaandeId(){
+        assertThat(repository.findById(-1)).isNotPresent();
+    }
+}
